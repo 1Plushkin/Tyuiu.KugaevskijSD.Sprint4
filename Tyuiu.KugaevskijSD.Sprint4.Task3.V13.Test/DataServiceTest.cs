fcb7fac0805aa204dcf9c6cc2da50790f6350611
1Plushkin <Plushkin001@mail.ru>
2025-11-11 00:58:11 +0500

    3

---
+++ Tyuiu.KugaevskijSD.Sprint4.Task3.V13.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.KugaevskijSD.Sprint4.Task3.V13.Lib;
+namespace Tyuiu.KugaevskijSD.Sprint4.Task3.V13.Test;
+
+[TestClass]
+public class DataServiceTest
+{
+    [TestMethod]
+    public void Calculate_Test()
+    {
+        int[,] matrix = {
+            {6, 3, 5, 7, 5},
+            {3, 5, 7, 4, 5},
+            {5, 6, 4, 5, 4},
+            {7, 4, 7, 3, 3},
+            {4, 6, 5, 5, 6}
+        };
+        DataService dataService = new DataService();
+        int expectedSum = 44; 
+
+        int actualSum = dataService.Calculate(matrix);
+
+        Assert.AreEqual(expectedSum, actualSum);
+    }
+}
