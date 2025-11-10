5060d80abd9327695c2438b5c43d942a2193f0b4
1Plushkin <Plushkin001@mail.ru>
2025-11-11 00:38:11 +0500

    2

---
+++ Tyuiu.KugaevskijSD.Sprint4.Task2.V21.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.KugaevskijSD.Sprint4.Task2.V21.Lib;
+namespace Tyuiu.KugaevskijSD.Sprint4.Task2.V21.Test;
+
+[TestClass]
+public class DataServiceTest
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
