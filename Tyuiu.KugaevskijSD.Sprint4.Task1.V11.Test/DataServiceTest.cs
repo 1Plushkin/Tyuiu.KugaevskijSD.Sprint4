60952a34867bce265f5ec6d00b3acc6f038e49e2
1Plushkin <Plushkin001@mail.ru>
2025-11-11 00:24:04 +0500

    1

---
+++ Tyuiu.KugaevskijSD.Sprint4.Task1.V11.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.KugaevskijSD.Sprint4.Task1.V11.Lib;
+namespace Tyuiu.KugaevskijSD.Sprint4.Task1.V11.Test;
+
+[TestClass]
+public class DataServiceTest
+{
+    [TestMethod]
+    public void ValidCalculate()
+    {
+        DataService ds = new DataService();
+        int[] array = new int[] { 2, 6, 5, 5, 1, 5, 6, 5, 5, 5, 1, 5, 3, 6, 6, 1, 6 };
+        int wait = 32;
+        int res = ds.Calculate(array);
+        Assert.AreEqual(wait, res);
+    }
+}
