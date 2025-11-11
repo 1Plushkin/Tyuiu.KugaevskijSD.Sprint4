eba40d435d7293952c59d47827b979c482997d72
1Plushkin <Plushkin001@mail.ru>
2025-11-11 21:19:41 +0500

    6

---
+++ Tyuiu.KugaevskijSD.Sprint4.Task6.V9.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.KugaevskijSD.Sprint4.Task6.V9.Lib;
+namespace Tyuiu.KugaevskijSD.Sprint4.Task6.V9.Test;
+
+[TestClass]
+public class DataServiceTest
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        DataService ds = new DataService();
+        string[] mas = { "Красный", "Оранжевый", "Желтый", "Зеленый", "Синий", "Индиго", "Фиолетовый" };
+        int wait = 3;
+        int res = ds.Calculate(mas);
+        Assert.AreEqual(wait, res);
+    }
+}
---
+++ Tyuiu.KugaevskijSD.Sprint4.Task6.V9/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KugaevskijSD.Sprint4.Task6.V9.Lib;
+
+namespace Tyuiu.KugaevskijSD.Sprint4.Task6.V9;
+
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.WriteLine("---------------------------------------------------------------------------");
+        Console.WriteLine("- ИСХОДНЫЕ ДАННЫЕ:                                                        -");
+        Console.WriteLine("---------------------------------------------------------------------------");
+
+
+        string[] mas = { "Красный", "Оранжевый", "Желтый", "Зеленый", "Синий", "Индиго", "Фиолетовый" };
+        Console.WriteLine("Массив:");
+        for (int i = 0; i < mas.Length; i++)
+        {
+            Console.WriteLine(mas[i]);
+        }
+
+        Console.WriteLine("---------------------------------------------------------------------------");
+        Console.WriteLine("- РЕЗУЛЬТАТ:                                                              -");
+        Console.WriteLine("---------------------------------------------------------------------------");
+
+        int res = ds.Calculate(mas);
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
+    
