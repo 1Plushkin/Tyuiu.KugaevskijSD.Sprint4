dc53adb7d654c50e3d9677e414916fd52a4cd3de
1Plushkin <Plushkin001@mail.ru>
2025-11-11 21:43:08 +0500

    7

---
+++ Tyuiu.KugaevskijSD.Sprint4.Task7.V8.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.KugaevskijSD.Sprint4.Task7.V8.Lib;
+namespace Tyuiu.KugaevskijSD.Sprint4.Task7.V8.Test;
+
+[TestClass]
+public class DataServiceTest
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        DataService ds = new DataService();
+        int rows = 3;
+        int columns = 4;
+        int[,] mtrx = new int[rows, columns];
+        string str = "264795863157";
+        int res = ds.Calculate(rows, columns, str);
+    }
+}
---
+++ Tyuiu.KugaevskijSD.Sprint4.Task7.V8/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KugaevskijSD.Sprint4.Task7.V8.Lib;
+
+namespace Tyuiu.KugaevskijSD.Sprint4.Task7.V8;
+    public class Program
+{
+    static void Main(string[] args)
+    {
+        int n = 3;
+        int m = 4;
+        int[,] mtrx = new int[n, m];
+
+        string str = "264795863157";
+
+        DataService ds = new DataService();
+        Console.Title = "Спринт #4 | Выполнила: Кугаевский С. Д. | АСОиУб-25-1";
+        Console.WriteLine("---------------------------------------------------------------------------");
+        Console.WriteLine("- Спринт #4                                                               -");
+        Console.WriteLine("- Тема: Добавление к решению итоговых проектов по спринту                 -");
+        Console.WriteLine("- Задание #7                                                              -");
+        Console.WriteLine("- Вариант #10                                                             -");
+        Console.WriteLine("- Выполнила: Кугаевский Сергей Дмитриевич | АСОиУб-25-1                   -");
+        Console.WriteLine("---------------------------------------------------------------------------");
+        Console.WriteLine("- УСЛОВИЕ:                                                                -");
+        Console.WriteLine("- Дана строка из одноразрядных цифр 264795863157 Преобразуйте ее в        -");
+        Console.WriteLine("- матрицу 3 на 4 и подсчитайте произведение нечетных чисел в матрице.     -");
+        Console.WriteLine("-                                                                         -");
+        Console.WriteLine("---------------------------------------------------------------------------");
+        Console.WriteLine("- ИСХОДНЫЕ ДАННЫЕ:                                                        -");
+        Console.WriteLine("---------------------------------------------------------------------------");
+
+        int index = 0;
+
+        Console.WriteLine("\nМассив:");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                Console.Write($"{str[index]} \t");
+                index++;
+
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+
+        Console.WriteLine("---------------------------------------------------------------------------");
+        Console.WriteLine("- РЕЗУЛЬТАТ:                                                              -");
+        Console.WriteLine("---------------------------------------------------------------------------");
+
+        int res = ds.Calculate(n, m, str);
+
+        Console.WriteLine("Произведение нечетных элементов = " + res);
+        Console.ReadKey();
+    }
+}
